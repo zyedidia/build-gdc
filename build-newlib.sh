d852e2545a9b772185a04ecc901710550a9edd7c
Zachary Yedidia <zyedidia@gmail.com>
2023-02-24 00:20:48 -0800

    Add newlib build script

---
+++ build-newlib.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 TARGET HOST"
+    exit 1
+fi
+
+TARGET=$1
+HOST=$2
+
+mkdir newlib-$TARGET
+cd newlib-$TARGET
+CC=$TARGET-gcc CFLAGS="-DPREFER_SIZE_OVER_SPEED" ../newlib-cygwin/newlib/configure --host=$HOST --target=$TARGET --disable-newlib-io-float
+make -j$(nproc --all)
