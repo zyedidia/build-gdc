404a5dfbd53810caafe2090aa5aa66c85084fe79
Zachary Yedidia <zyedidia@gmail.com>
2023-02-24 11:28:43 -0800

    Install headers too

--- build-newlib.sh
+++ build-newlib.sh
@@ -13,3 +13,6 @@ mkdir newlib-$TARGET
 cd newlib-$TARGET
 CC=$TARGET-gcc CFLAGS="-DPREFER_SIZE_OVER_SPEED" ../newlib-cygwin/newlib/configure --host=$HOST --target=$TARGET --disable-newlib-io-float
 make -j$(nproc --all)
+
+mkdir include
+cp -r ../newlib-cygwin/newlib/libc/include ./include/libc
